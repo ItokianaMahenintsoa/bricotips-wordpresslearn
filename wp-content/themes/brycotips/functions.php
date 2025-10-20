ad49747facb9f6d26d4cb11ad85830c36ac34be3
fy.mahenintsoa <mahenintsoafyitokiana@gmail.com>
2025-10-20 10:27:13 +0300

    Create and Add shortcode

--- wp-content/themes/brycotips/functions.php
+++ wp-content/themes/brycotips/functions.php
@@ -15,6 +15,9 @@ function bricotips_child_enqueue_styles()
     );
 
     wp_enqueue_style('image-titre-widget', get_stylesheet_directory_uri() . '/css/widgets/image-titre-widget.css', array(), filemtime(get_stylesheet_directory() . '/css/widgets/image-titre-widget.css'));
+
+    // Short Code Banniere Titre
+    wp_enqueue_style('banniere-titre-shortcode', get_stylesheet_directory_uri() . '/css/shortcodes/banniere-titre.css', array(), filemtime(get_stylesheet_directory() . '/css/shortcodes/banniere-titre.css'));
 }
 add_action('wp_enqueue_scripts', 'bricotips_child_enqueue_styles');
 
@@ -31,3 +34,33 @@ function register_widgets()
 }
 //On demander à wordpress de charger des widget selon la fonction register_widgets()
 add_action('widgets_init', 'register_widgets');
+
+add_shortcode('banniere-titre', 'banniere_titre_func');
+// Je génère le html retourné par mon shortcode
+function banniere_titre_func($atts)
+{
+    //Je récupère les attributs mis sur le shortcode
+    $atts = shortcode_atts(array(
+        'src' => '',
+        'titre' => 'Titre'
+    ), $atts, 'banniere-titre');
+
+    //Je commence à récupéré le flux d'information
+    ob_start();
+
+    if ($atts['src'] != "") {
+        ?>
+
+        <div class="banniere-titre" style="background-image: url(<?= $atts['src'] ?>)">
+            <h2 class="titre"><?= $atts['titre'] ?></h2>
+        </div>
+
+        <?php
+    }
+
+    //J'arrête de récupérer le flux d'information et le stock dans la fonction $output
+    $output = ob_get_contents();
+    ob_end_clean();
+
+    return $output;
+}
