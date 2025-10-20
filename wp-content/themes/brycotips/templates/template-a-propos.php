166b3b3ac7630c1dc693573266cb06e640909e6d
fy.mahenintsoa <mahenintsoafyitokiana@gmail.com>
2025-10-20 16:02:35 +0300

    Create new template model

---
+++ wp-content/themes/brycotips/templates/template-a-propos.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Template Name: template-a-propos
+ */
+
+get_header();
+?>
+
+<main id="site-content">
+    Nous sommes dans la page à propos
+
+</main><!-- #site-content -->
+
+<?php get_template_part( 'template-parts/footer-menus-widgets' ); ?>
+
+<?php
+get_footer();
